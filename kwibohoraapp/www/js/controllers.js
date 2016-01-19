9bd899a43c2dc4a23a4ad36098b6fe30ee2491ee
loic shyaka <loicshyaka09@gmail.com>
2016-01-19 16:57:43 +0800

    Single day event Ionic chat app

--- kwibohoraapp/www/js/controllers.js
+++ kwibohoraapp/www/js/controllers.js
@@ -3,45 +3,6 @@ angular.module('starter.controllers', [])
 .controller('HomeCtrl', function($scope) {})
 
 
-
-// .controller('VotesCtrl',['$scope', '$http', function($scope, $http, $state, $ionicPopup) {
-//   var url = 'http://0f23370d.ngrok.io/candidates.json';
-//   $http.get(url).success(function(data){
-//       $scope.canditates = data;  
-
-//       $scope.doRefresh = function(){
-//         $http.get(url).success(function(data){
-//           $scope.candidates = data; 
-//           $scope.$broadcast('scroll.refreshComplete');
-//         });
-//       }
-
-      // $scope.sendVote = function(vote){
-      // console.log("hey");
-      // // var hasVoted = window.localStorage.getItem(LOCAL_TOKEN_KEY3);
-      // // if( !has_voted ) {
-      // //    var url = 'http://0f23370d.ngrok.io/votes';
-      // //    $http.post(url, {user_id: 1, candidate_id: vote.value}).success(function() {
-      // //        window.localStorage.setItem(LOCAL_TOKEN_KEY3, true);
-      // //        $state.go('tab.dash');
-      // //        var alertPopup = $ionicPopup.alert({
-      //          title: 'Thank You!',
-      //          template: 'Your vote has been sent.'
-      //        });
-
-      //       }).
-
-      //       error(function(data) {
-      //         // called asynchronously if an error occurs
-      //         // or server returns response with an error status.
-      //       });
-//       // }
-     
-//       }
-      
-//    });
-// }])
-
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
